b3e873236499b2d908e6adffa1e2462f9987492e
rizkyadiryanto <73731176+Rizky1408@users.noreply.github.com>
2021-09-09 07:44:57 -0700

    Add files via upload

---
+++ lib/screens/search_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+typedef OnSearchChanged = Future<List<String>> Function(String);
+
+class SearchWithSuggestionDelegate extends SearchDelegate<String> {
+  ///[onSearchChanged] gets the [query] as an argument. Then this callback
+  ///should process [query] then return an [List<String>] as suggestions.
+  ///Since its returns a [Future] you get suggestions from server too.
+  final OnSearchChanged onSearchChanged;
+
+  ///This [_oldFilters] used to store the previous suggestions. While waiting
+  ///for [onSearchChanged] to completed, [_oldFilters] are displayed.
+  List<String> _oldFilters = const [];
+
+  SearchWithSuggestionDelegate({String searchFieldLabel, this.onSearchChanged})
+      : super(searchFieldLabel: searchFieldLabel);
+
+  ///
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () => Navigator.pop(context),
+    );
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () => query = "",
+      ),
+    ];
+  }
+
+  ///OnSubmit in the keyboard, returns the [query]
+  @override
+  void showResults(BuildContext context) {
+    close(context, query);
+  }
+
+  ///Since [showResults] is overridden we can don't have to build the results.
+  @override
+  Widget buildResults(BuildContext context) => null;
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder<List<String>>(
+      future: onSearchChanged != null ? onSearchChanged(query) : null,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) _oldFilters = snapshot.data;
+        return ListView.builder(
+          itemCount: _oldFilters.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.restore),
+              title: Text("${_oldFilters[index]}"),
+              onTap: () => close(context, _oldFilters[index]),
+            );
+          },
+        );
+      },
+    );
+  }
+}
