f5f7d021b0bfe82d63f0d4efcfc0982280e6a425
rizkyadiryanto <73731176+Rizky1408@users.noreply.github.com>
2021-09-09 07:46:10 -0700

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'screens/explore_screen.dart';
 import 'screens/recipes_screen.dart';
 import 'screens/grocery_screen.dart';
+import 'screens/search_bar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -11,6 +13,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Future<void> _showSearch() async {
+    final searchText = await showSearch<String>(
+      context: context,
+      delegate: SearchWithSuggestionDelegate(
+        onSearchChanged: _getRecentSearchesLike,
+      ),
+    );
+    await _saveToRecentSearches(searchText);
+  }
+
+  Future<List<String>> _getRecentSearchesLike(String query) async {
+    final pref = await SharedPreferences.getInstance();
+    final allSearches = pref.getStringList("recentSearches");
+    return allSearches.where((search) => search.startsWith(query)).toList();
+  }
+
+  Future<void> _saveToRecentSearches(String searchText) async {
+    if (searchText == null) return;
+    final pref = await SharedPreferences.getInstance();
+
+    Set<String> allSearches =
+        pref.getStringList("recentSearches")?.toSet() ?? {};
+
+    allSearches = {searchText, ...allSearches};
+    pref.setStringList("recentSearches", allSearches.toList());
+  }
+
   int _selectedIndex = 0;
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
@@ -27,8 +56,12 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text('Fooderlich',
-                style: Theme.of(context).textTheme.headline6)),
+          title:
+              Text('Fooderlich', style: Theme.of(context).textTheme.headline6),
+          actions: <Widget>[
+            IconButton(onPressed: _showSearch, icon: Icon(Icons.search))
+          ],
+        ),
         body: pages[_selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
             selectedItemColor:
